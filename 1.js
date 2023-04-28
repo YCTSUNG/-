b31ae0915cb97eb68288b2313af108b478fcfb06
YCTSUNG <20tka060@esp-g.com>
2023-04-28 13:05:26 +0900

    補上修改店家

--- 1.js
+++ 1.js
@@ -2,9 +2,13 @@ let input1 = document.querySelector("#input1");
 let input2 = document.querySelector("#input2");
 let input3 = document.querySelector("#input3");
 let input4 = document.querySelector("#input4");
+let input5 = document.querySelector("#input5");
+let input6 = document.querySelector("#input6");
+let input7 = document.querySelector("#input7");
 
 let btn1 = document.querySelector(".btn1")
 let btn4 = document.querySelector(".btn4")
+let btn5 = document.querySelector(".btn5")
 
 
 btn1.addEventListener("click" , () =>{
@@ -59,3 +63,31 @@ btn1.addEventListener("click" , () =>{
         
         input4.value = "";
         })
+
+        btn5.addEventListener("click" , () =>{
+
+            let body ={
+                "gourmet_map_list": [{
+                    "dinerName" : input5.value,
+                    "city" : input6.value,     
+                    "dinerScore" : input7.value
+                } ]
+            }
+            fetch("http://localhost:8080/update_diner", {
+            method:"POST",
+            headers:{"Content-Type" : "application/json",
+            },
+            body: JSON.stringify(body)
+            })
+            .then(response=>{
+                return response.json()
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => console.log(err))
+            
+            input5.value = "";
+            input6.value = "";
+            input6.value = "";
+            })
